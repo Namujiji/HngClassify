954029ba387441f3822aff85084272ddaea1311a
Abdullahi Shuru <abdullahishuru@gmail.com>
2026-04-18 23:03:38 +0100

    feat: update classify respnse json data property format

--- Application/Features/Genderize/Responses/ClassifyResponse.cs
+++ Application/Features/Genderize/Responses/ClassifyResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace HngClassify.Application.Features.Genderize.Responses;
+﻿using System.Text.Json.Serialization;
+
+namespace HngClassify.Application.Features.Genderize.Responses;
 
 public record ClassifyResponse(
     string Status,
@@ -15,12 +17,23 @@ public interface IClassifyResponse
     string Status { get; init; }
 }
 
-    public record Data
+public record Data
 {
+    [JsonPropertyName("name")]
     public string? Name { get; init; }
+
+    [JsonPropertyName("gender")]
     public string? Gender { get; init; }
+
+    [JsonPropertyName("probability")]
     public double Probability { get; init; }
+
+    [JsonPropertyName("sample_size")]
     public int SampleSize { get; init; }
+
+    [JsonPropertyName("is_confident")]
     public bool IsConfident { get; init; }
+
+    [JsonPropertyName("processed_at")]
     public DateTime ProcessedAt { get; init; }
 }
